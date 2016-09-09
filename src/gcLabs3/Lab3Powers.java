857628d6426fd2d7f7bf30507eadac1b7ff7552f
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-09 00:56:05 -0400

    changes to formula; loops

--- src/gcLabs3/Lab3Powers.java
+++ src/gcLabs3/Lab3Powers.java
@@ -13,22 +13,29 @@ public class Lab3Powers {
 		
 		
 		String choice;
-		choice = "yes";
+		choice = "y";
 		
 		//sysout and user prompts
 		System.out.println("Learn your squares and cubes!");
 		System.out.println();
 		
+		do{
+			
 		System.out.println("Enter an integer between 1 and 50:");
 		k = new Scanner(System.in);
 		number = k.nextInt();
 		
-		do{
 			System.out.println(" NUMBER        " + " SQUARED       " + " CUBED " );
 			System.out.println("========       " + "=========      " + "=======");
-			for(int i = 0; i <= 50; i++);
-			System.out.println("  "+(number) +"                 "+(number * number)+ "            "+(number * number * number));
-
+		
+			for(int i = 0; i <=50; i++){
+			System.out.println("  "+(i) +"             "+(i * number)+ "         "+(i * number * number));
+			}
+			System.out.println();
+			System.out.println("Continue? y/n: ");
+			k.nextLine();
+			
+			choice = k.nextLine();
 		}while (choice.equals("y"));
 		
 	}
