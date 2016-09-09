fba9f4785737f4891b56cf9d4aade68accf9efb1
Kimberli Smith <kimsmith.it@gmail.com>
2016-09-09 00:32:52 -0400

    set up visuals and formulas

--- src/gcLabs3/Lab3Powers.java
+++ src/gcLabs3/Lab3Powers.java
@@ -10,8 +10,7 @@ public class Lab3Powers {
 		// TODO Auto-generated method stub
 		//variables
 		int number;
-		int squared;
-		int cubed;
+		
 		
 		String choice;
 		choice = "yes";
@@ -20,13 +19,16 @@ public class Lab3Powers {
 		System.out.println("Learn your squares and cubes!");
 		System.out.println();
 		
-		System.out.println("Enter an integer:");
+		System.out.println("Enter an integer between 1 and 50:");
 		k = new Scanner(System.in);
 		number = k.nextInt();
 		
 		do{
-			System.out.println("     ");
-			
+			System.out.println(" NUMBER        " + " SQUARED       " + " CUBED " );
+			System.out.println("========       " + "=========      " + "=======");
+			for(int i = 0; i <= 50; i++);
+			System.out.println("  "+(number) +"                 "+(number * number)+ "            "+(number * number * number));
+
 		}while (choice.equals("y"));
 		
 	}
